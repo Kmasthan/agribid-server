01199d118b27207c84d5bb9331734db409036f97
Masthanaiah K <masthankalthireddy@gmail.com>
2025-05-28 18:20:21 +0530

    initial commit

---
+++ src/main/java/com/agribid_server/AgribidServerApplication.java
@@ -0,0 +1,13 @@
+package com.agribid_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgribidServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgribidServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agribid_server/AgribidServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agribid_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgribidServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
